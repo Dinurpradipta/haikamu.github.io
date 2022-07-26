bc6f541c1bdd10562bfe7301bc87bb376be06a88
Dinurpradipta <110051580+Dinurpradipta@users.noreply.github.com>
2022-07-27 01:34:16 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function time() {
   var d = new Date();
   var n = d.getHours();
   console.log(n);
-  if (n >= 5 && n <= 10) {
+  if (n >= 24 && n <= 10) {
     return "Pagi"
   } else if (n >= 10 && n <= 15) {
     return "Siang"
